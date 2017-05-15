55c82657bd9980e23bbb558b889266fcfa6eaef8
JooYoungHeo <jyhur900226@naver.com>
2017-05-15 19:18:35 +0900

    [작업] RequestMapping - Controller 저장 구현

--- src/main/java/webserver/RequestMapping.java
+++ src/main/java/webserver/RequestMapping.java
@@ -1,12 +1,26 @@
 package webserver;
 
 import controller.Controller;
+import controller.CreateUserController;
+import controller.ListUserController;
+import controller.LoginController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by heojooyoung on 2017. 5. 15..
  */
 public class RequestMapping {
-    public static Map<String, Controller> controllerMap = new Map<String, Controller>();
+    private static Map<String, Controller> controllerMap = new HashMap<String, Controller>();
+
+    void tmp(){
+        controllerMap.put("/user/create", new CreateUserController());
+        controllerMap.put("/user/list", new ListUserController());
+        controllerMap.put("/user/login", new LoginController());
+    }
+
+    public Controller getController(String url){
+        return controllerMap.get(url);
+    }
 }
