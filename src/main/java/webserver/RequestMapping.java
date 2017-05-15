8b70ae731979e47cd5119568924e0b840e112bb1
JooYoungHeo <jyhur900226@naver.com>
2017-05-15 19:12:27 +0900

    [작업] RequestMapping 생성

---
+++ src/main/java/webserver/RequestMapping.java
@@ -0,0 +1,12 @@
+package webserver;
+
+import controller.Controller;
+
+import java.util.Map;
+
+/**
+ * Created by heojooyoung on 2017. 5. 15..
+ */
+public class RequestMapping {
+    public static Map<String, Controller> controllerMap = new Map<String, Controller>();
+}
